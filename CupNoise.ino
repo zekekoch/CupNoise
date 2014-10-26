3980a3ba5a7e7a94492b23d65026a9b0e4915378
Zeke Koch <zekekoch@Selinas-MacBook-Air.local>
2014-10-26 10:46:06 -0700

    CupHead (with FHT)

--- CupNoise.ino
+++ CupNoise.ino
@@ -1,7 +1,8 @@
+#define LOG_OUT 1 // use the log output function
+#define FHT_N 64 // set to 256 point fht
+#include <FHT.h> // include the library
 #include<FastLED.h>
 
-// How many leds are in the strip?
-#define numLeds 146
 
 #define pin1 8
 #define pin2 9
@@ -15,6 +16,13 @@
 #define strip4 41
 #define strip5 17
 
+#define MIC_PIN 0                                             // Analog port for microphone
+#define DC_OFFSET  0                                         // DC offset in mic signal - if unusure, leave 0
+
+const int sampleWindow = 50; // Sample window width in mS (50 mS = 20Hz)
+unsigned int sample;
+
+
 const uint8_t kColumns = 23;
 const uint8_t kRows = 9;
 const uint8_t kMatrixWidth = kColumns;
@@ -23,6 +31,7 @@ byte inByte = 0;
 boolean serialComplete = false;
 
 #define NUM_LEDS (kColumns * kRows)
+#define numLeds NUM_LEDS
 
 //
 // Mark's xy coordinate mapping code.  See the XYMatrix for more information on it.
@@ -30,8 +39,6 @@ boolean serialComplete = false;
 
 // Params for width and height
 #define MAX_DIMENSION ((kMatrixWidth>kMatrixHeight) ? kMatrixWidth : kMatrixHeight)
-// Param for different pixel layouts
-const bool    kMatrixSerpentineLayout = true;
 
 // This function will return the right 'led index number' for 
 // a given set of X and Y coordinates on your RGB Shades. 
@@ -44,15 +51,16 @@ uint16_t XY( uint8_t x, uint8_t y)
 
   const uint16_t mappedTable[kRows * kColumns] = 
   {
-24, 24, 24, 24, 24, 24, 25, 25, 25, 25, 25, 25, 25, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
-20, 19, 19, 18, 18, 18, 17, 17, 16, 16, 16, 15, 15, 14, 14, 14, 22, 21, 21, 21, 21, 20, 20,
-8,  7,  7,  6,  6,  5,  4,  4,  3,  3,  2,  1,  1,  0,  0,  13, 12, 11, 11, 10, 10, 9,  9,
-57, 56, 55, 55, 54, 52, 51, 51, 50, 50, 49, 48, 48, 47, 64, 63, 62, 62, 61, 60, 59, 59, 58,
-37, 36, 35, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 46, 45, 44, 43, 42, 41, 40, 39, 39, 38,
-76, 75, 74, 73, 72, 71, 70, 69, 68, 67, 66, 65, 87, 86, 85, 84, 83, 82, 81, 80, 79, 78, 77,
-115, 114, 113,  112,  111,  110,  109,  108,  107,  106,  105,  126,  125,  125,  124,  123,  122,  121,  120,  119,  118,  117,  116,  
-94, 93, 92, 91, 90, 89, 88, 88, 144,  145,  104,  103,  102,  101,  101,  100,  99, 99, 98, 97, 96, 95, 94, 
-135,  134,  133,  133,  132,  131,  131,  131,  130,  129,  128,  129,  127,  143,  142,  141,  140,  140,  138,  138,  137,  136, 135,  
+//        0,   1,    2,    3,    4,    5,    6,    7,    8,    9,    10,   11,   12,   13,   14,   15,   16,   17,   18,   19,   20,   21,   22,    
+/* 0 */  24,   24,   24,   24,   24,   24,   25,   25,   25,   25,   25,   25,   25,   25,   23,   23,   23,   23,   23,   23,   23,   25,   25,
+/* 1 */  20,   19,   19,   18,   18,   18,   17,   17,   16,   16,   16,   15,   15,   14,   14,   14,   22,   22,   22,   21,   21,   21,   20,
+/* 2 */  8,    7,    7,    6,    6,    5,    4,    4,    3,    3,    2,    1,    1,    0,    0,    13,   12,   12,   11,   10,   10,   9,    9,
+/* 3 */  57,   56,   55,   54,   54,   53,   52,   52,   51,   50,   49,   48,   48,   47,   64,   63,   62,   62,   61,   60,   59,   59,   58,
+/* 4 */  37,   36,   35,   34,   33,   32,   31,   31,   30,   29,   28,   27,   26,   46,   45,   44,   43,   42,   41,   40,   39,   39,   38,
+/* 5 */  76,   75,   74,   73,   72,   71,   70,   69,   68,   67,   66,   65,   87,   86,   85,   84,   83,   82,   81,   80,   79,   78,   77,
+/* 6 */  115,  114,  113,  112,  111,  110,  109,  108,  107,  106,  105,  126,  125,  124,  123,  122,  121,  121,  120,  119,  118,  117,  116,  
+/* 7 */  94,   93,   92,   91,   90,   89,   88,   88,   145,  144,  104,  103,  102,  101,  100,  100,  99,   99,   98,   97,   96,   95,   94, 
+/* 8 */  135,  134,  133,  132,  131,  130,  130,  130,  129,  143,  142,  141,  141,  140,  139,  139,  138,  138,  137,  137,  136,  135,  134,  
   };
 
   // lazy bounds checking
@@ -74,89 +82,32 @@ uint16_t XY( uint8_t x, uint8_t y)
 
 }
 
-uint16_t oldXY( uint8_t x, uint8_t y)
-{
-  uint16_t i;
-
-  if( kMatrixSerpentineLayout == false) {
-    i = (y * kMatrixWidth) + x;
-  }
-
-  if( kMatrixSerpentineLayout == true) {
-    if( y & 0x01) {
-      // Odd rows run backwards
-      uint8_t reverseX = (kMatrixWidth - 1) - x;
-      i = (y * kMatrixWidth) + reverseX;
-    } else {
-      // Even rows run forwards
-      i = (y * kMatrixWidth) + x;
-    }
-  }
-
-  return i;
-}
-
 // The leds
 CRGB leds[kMatrixWidth * kMatrixHeight];
-
-// The 32bit version of our coordinates
-static uint16_t x;
-static uint16_t y;
-static uint16_t z;
-
-// We're using the x/y dimensions to map to the x/y pixels on the matrix.  We'll
-// use the z-axis for "time".  speed determines how fast time moves forward.  Try
-// 1 for a very slow moving effect, or 60 for something that ends up looking like
-// water.
-// uint16_t speed = 1; // almost looks like a painting, moves very slowly
-uint16_t speed = 20; // a nice starting speed, mixes well with a scale of 100
-// uint16_t speed = 33;
-// uint16_t speed = 100; // wicked fast!
-
-// Scale determines how far apart the pixels in our noise matrix are.  Try
-// changing these values around to see how it affects the motion of the display.  The
-// higher the value of scale, the more "zoomed out" the noise iwll be.  A value
-// of 1 will be so zoomed in, you'll mostly see solid colors.
-
-// uint16_t scale = 1; // mostly just solid colors
-// uint16_t scale = 4011; // very zoomed out and shimmery
-uint16_t scale = 311;
-
-// This is the array that we keep our computed noise values in
-uint8_t noise[MAX_DIMENSION][MAX_DIMENSION];
+bool drops[kMatrixWidth][kMatrixHeight] = {false};
 
 void setup() {
   // uncomment the following lines if you want to see FPS count information
   // Serial.begin(38400);
   // Serial.println("resetting!");
+  pinMode(A0, INPUT);
   delay(200);
   LEDS.addLeds<WS2812B, pin1, GRB>(leds, strip1).setCorrection(TypicalSMD5050);
   LEDS.addLeds<WS2812B, pin2, GRB>(leds, strip1, strip2).setCorrection(TypicalSMD5050);
   LEDS.addLeds<WS2812B, pin3, GRB>(leds, strip1 + strip2, strip3).setCorrection(TypicalSMD5050);
   LEDS.addLeds<WS2812B, pin4, GRB>(leds, strip1 + strip2 + strip3, strip4).setCorrection(TypicalSMD5050);
   LEDS.addLeds<WS2812B, pin5, GRB>(leds, strip1 + strip2 + strip3 + strip4, strip5).setCorrection(TypicalSMD5050);
+  FastLED.setBrightness(255);
+  set_max_power_in_volts_and_milliamps(5, 3000);
+  setAllColor(CRGB::Black);
 
-  LEDS.setBrightness(255);
-  Serial.begin(14400);
+  Serial.begin(115200);
 
-  // Initialize our coordinates to some random values
-  x = random16();
-  y = random16();
-  z = random16();
+  TIMSK0 = 0; // turn off timer0 for lower jitter
+  ADCSRA = 0xe5; // set the adc to free running mode
+  ADMUX = 0x40; // use adc0
+  DIDR0 = 0x01; // turn off the digital input for adc0
 }
-
-// Fill the x/y array of 8-bit noise values using the inoise8 function.
-void fillnoise8() {
-  for(int i = 0; i < MAX_DIMENSION; i++) {
-    int ioffset = scale * i;
-    for(int j = 0; j < MAX_DIMENSION; j++) {
-      int joffset = scale * j;
-      noise[i][j] = inoise8(x + ioffset,y + joffset,z);
-    }
-  }
-  z += speed;
-}
-
 
 void simpleTest() 
 {
@@ -179,7 +130,7 @@ void setAllColor(CRGB color)
 {
   for(int col = 0;col<kColumns;col++)
   {
-    for (int row = 0;row<kColumns;row++)
+    for (int row = 0;row<kRows;row++)
     {
       leds[XY(col, row)] = color;
     }
@@ -204,16 +155,29 @@ void setColumnColor(byte column, CRGB color)
   }
 }
 
+// takes an 8-bit height and maps it to column
+void setColumnHeight(byte column, byte height, CRGB color)
+{
+  for(int row = 0;row<kRows;row++)
+  {
+    if (row < height/9  )
+      leds[XY(column,row)] = color;
+    else
+      leds[XY(column,row)].fadeToBlackBy( 64 );
+  }
+
+}
+
 void columnWipe()
 {
   static byte hue = 0;
   for(int col = 0;col < kColumns;col++)
   {
-    hue++;
-    setColumnColor(col, hue);
+    hue += 20;
+    setColumnColor(col, CHSV(hue,255,255));
+    show_at_max_brightness_for_power();
+    delay(50);
   }
-  LEDS.show();
-  delay(50);
 }
 
 void setLedFromSerial()
@@ -225,7 +189,7 @@ void setLedFromSerial()
     leds[inByte] = CRGB::Fuchsia;
     inByte = 0;
     serialComplete = false;
-    FastLED.show();
+    show_at_max_brightness_for_power();
       //setRowColor(i, CRGB::Black);
   }
 }
@@ -242,22 +206,184 @@ void setColFromSerial()
     leds[inByte] = CRGB::Blue;
     inByte = 0;
     serialComplete = false;
-    FastLED.show();
-      //setRowColor(i, CRGB::Black);  
+    show_at_max_brightness_for_power();
+    //setRowColor(i, CRGB::Black);  
   }
 }
 
-
-void loop()
+uint32_t getAmplitude()
 {
-  byte hue = 0;
-  LEDS.showColor(CRGB::Black);
-  //setLedFromSerial();
-  setColFromSerial();
-  LEDS.show();
-  //delay(5000);
+   unsigned long startMillis= millis();  // Start of sample window
+   unsigned int peakToPeak = 0;   // peak-to-peak level
+ 
+   unsigned int signalMax = 0;
+   unsigned int signalMin = 1024;
+ 
+   // collect data for 50 mS
+   while (millis() - startMillis < sampleWindow)
+   {
+      sample = analogRead(0);
+      if (sample < 1024)  // toss out spurious readings
+      {
+         if (sample > signalMax)
+         {
+            signalMax = sample;  // save just the max levels
+         }
+         else if (sample < signalMin)
+         {
+            signalMin = sample;  // save just the min levels
+         }
+      }
+   }
+   peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
+ 
+   Serial.println(peakToPeak);
+   return peakToPeak;
+}
 
-  /*
+byte hue = 0;
+
+void loop() {
+  static unsigned long nextChange = millis() + 50;
+
+  
+  // loop forever
+  while(true)
+  {
+    getFHT();
+    //Serial.write(255); // send a start byte
+    //Serial.write(fht_log_out, FHT_N/2); // send out the data/*
+
+    //waterDroplets();
+      doLeds();
+      LEDS.show(); 
+      LEDS.delay(20);
+  }
+  LEDS.delay(20);
+}
+
+void getFHT()
+{
+  cli();  // UDRE interrupt slows this way down on arduino1.0
+  for (int i = 0 ; i < FHT_N ; i++) { // save 256 samples
+    while(!(ADCSRA & 0x10)); // wait for adc to be ready
+    ADCSRA = 0xf5; // restart adc
+    byte m = ADCL; // fetch adc data
+    byte j = ADCH;
+    int k = (j << 8) | m; // form into an int
+    k -= 0x0200; // form into a signed int
+    k <<= 6; // form into a 16b signed int
+    fht_input[i] = k; // put real data into bins
+  }
+  fht_window(); // window the data for better frequency response
+  fht_reorder(); // reorder the data before doing the fht
+  fht_run(); // process the data in the fht
+  fht_mag_log(); // take the output of the fht
+  sei();
+}
+
+const byte changeDuration = 50;
+void doLeds()
+{
+  static byte hue = 0;
+  for(int col = 0;col < kColumns;col++)
+  {
+    if (fht_log_out[0] > 128)
+    {
+      hue += 10;
+    }
+  //Serial.print(col);Serial.print(".");Serial.print(fht_log_out[col]);Serial.print(", ");
+    //hue += 10;
+    setColumnHeight(col, fht_log_out[col+2], CHSV(hue,255,255));
+  }
+  //Serial.println();
+}
+
+void waterDroplets()
+{
+  // first seed the top row with a droplet
+  byte firstDrop = random8(0,22);
+
+  // i use a seperate drop array because the head is a globe
+  // and so leds has a lot of duplicates in it
+  for(int i = 0;i<kColumns;i++)
+  {
+    drops[i][0] =false;
+  }
+  drops[firstDrop][0] = true;
+
+
+  // I'm just copying (from the bottom) a row down
+  for(int row = kRows -1;row > 0;row--)
+  {
+    for (int col = kColumns-1;col >= 0;col--)
+    {
+      drops[col][row] = drops[col][row-1];
+    }    
+  }
+
+  for(int row = kRows -1;row >= 0;row--)
+  {
+    for (int col = kColumns-1;col >= 0;col--)
+    {
+      if(drops[col][row] == true)
+      {
+        leds[XY(col,row)] = CRGB::Blue;
+      } 
+      else
+      {
+        leds[XY(col,row)] /= 2;        
+      }
+    }    
+  }
+  Serial.println("dropped");
+}
+
+
+/*
+void looped()
+{
+  byte fhtresults[kColumns];
+  byte hue = 0;
+
+  cli();  // UDRE interrupt slows this way down on arduino1.0
+  for (int i = 0 ; i < FHT_N ; i++) { // save 256 samples
+    while(!(ADCSRA & 0x10)); // wait for adc to be ready
+    ADCSRA = 0xf5; // restart adc
+    byte m = ADCL; // fetch adc data
+    byte j = ADCH;
+    int k = (j << 8) | m; // form into an int
+    k -= 0x0200; // form into a signed int
+    k <<= 6; // form into a 16b signed int
+    fht_input[i] = k; // put real data into bins
+  }
+  fht_window(); // window the data for better frequency response
+  fht_reorder(); // reorder the data before doing the fht
+  fht_run(); // process the data in the fht
+  fht_mag_log(); // take the output of the fht
+  for(int i = 0;i< kColumns;i++)
+  {
+    fhtresults[i] = fht_log_out[i]; // send out the data
+  }  
+  sei();
+
+  for(int i = 0;i< kColumns;i++)
+  {
+    hue = fhtresults[i];
+    setColumnColor(i, CHSV(hue,255,255));
+  }
+  //columnWipe();
+  //soundmems();
+  //Serial.println(LEDS.getFPS());
+
+  //getAmplitude();
+  //setAllColor(CRGB::White);
+  //setLedFromSerial();
+  //setColFromSerial();
+  LEDS.show();
+  //show_at_max_brightness_for_power();
+  //delay_at_max_brightness_for_power(60);
+
   LEDS.showColor(CRGB::Black);
   for (byte col = 0;col<kColumns;col++)
   {
@@ -268,22 +394,19 @@ void loop()
   //setSerial();
   LEDS.show();
   delay(10000);
-  */
 }
+*/
 
-void loopNoise() {
-  static uint8_t ihue=0;
-  fillnoise8();
-  for(int i = 0; i < kMatrixWidth; i++) {
-    for(int j = 0; j < kMatrixHeight; j++) {
-      leds[XY(i,j)] = CHSV(noise[j][i],255,noise[i][j]);
-    }
+
+void soundmems() {
+  int n;
+  for (int i = 0; i<NUM_LEDS; i++) {
+    n = analogRead(MIC_PIN);                                   // Raw reading from mic
+    n = abs(n - 512 - DC_OFFSET);                              // Center on zero
+    leds[i] = CHSV((n*2 % 255), 255, (n*2)% 255);
   }
-  ihue+=1;
+} // soundmems()
 
-  LEDS.show();
-  // delay(10);
-}
 
 void serialEvent() {
   while (Serial.available()) {
@@ -300,4 +423,5 @@ void serialEvent() {
     }
   }
 }
+
 
